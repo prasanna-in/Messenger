3cb44919ab2da410e429326d9bffe642532b91fe
Shifu <prasanna.in@gmail.com>
2016-07-07 14:07:22 +0800

    Chat...

--- Messenger.go
+++ Messenger.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"io/ioutil"
 	"encoding/json"
+	"reflect"
 )
 
 func HttpHandler2(response http.ResponseWriter, request *http.Request)  {
@@ -19,7 +20,7 @@ func HttpHandler2(response http.ResponseWriter, request *http.Request)  {
 	json.Unmarshal(body,&Telegramresponse)
 	text := Telegramresponse.Message.String()
 	log.Println(text)
-	log.Println("PM was Here")
+	log.Println(reflect.TypeOf(text))
 	if text == "/Register"{
 		log.Println("PK was Here")
 	}
