d9c51eda7ac7eec6636a4e6a77489937a215ce33
Shifu <prasanna.in@gmail.com>
2016-07-07 15:07:39 +0800

    Chat...

--- Messenger.go
+++ Messenger.go
@@ -27,7 +27,7 @@ func HttpHandler2(response http.ResponseWriter, request *http.Request)  {
 	case "/Register":
 		log.Println(Telegramresponse.Message.Chat.TGGroupChat)
 	default:
-		log.Println(Telegramresponse.Message.Chat.TGUser.Id)
+		log.Println(Telegramresponse.Message.Chat.TGUser)
 	}
 }
 func main() {
